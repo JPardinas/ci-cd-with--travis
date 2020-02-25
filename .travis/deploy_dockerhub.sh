a8d0bb86eda0addd4fd627425c9eccb38369d217
Jpardinas <jpardinas23@gmail.com>
2020-02-25 15:47:42 +0100

    try to fix uppercase

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -5,6 +5,6 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
 else
     TAG="$TRAVIS_BRANCH"
 fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:${TAG,,} .
 docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
